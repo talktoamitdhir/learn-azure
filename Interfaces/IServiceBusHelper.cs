9ca3da2cd7f57d8581f2422712c34dec0a16c15b
Ashwin Brahmarouthu <Ashwin.Brahmarouthu@technossus.com>
2018-10-13 16:44:34 -0700

    Added Keyvaults, Interfaces

---
+++ Interfaces/IServiceBusHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface IServiceBusHelper
+    {
+        void SendMessageToQueue();
+        void ReceiveMessageFromQueue();
+        void SendMessageToTopic();
+        void ReceiveMessageFromTopic();
+    }
+}
