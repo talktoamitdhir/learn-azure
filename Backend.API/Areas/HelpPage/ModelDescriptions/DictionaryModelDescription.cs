e804e225777ae74dbdcf50ffac43c700b0bb7847
Ashwin Brahmarouthu <Ashwin.Brahmarouthu@technossus.com>
2018-10-13 16:09:26 -0700

    Added WebAPI Project

---
+++ Backend.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Backend.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Backend.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Backend.API.WebApiApplication" Language="C#" %>
