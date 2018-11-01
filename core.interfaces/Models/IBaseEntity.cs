056de6b70c109d5f92ad139f33b36e426942b9d2
Ashwin Brahmarouthu <Ashwin.Brahmarouthu@technossus.com>
2018-11-01 16:05:46 -0700

    Merged ITrackEntity into IBaseEntity

--- core.interfaces/Models/IBaseEntity.cs
+++ core.interfaces/Models/IBaseEntity.cs
@@ -1,8 +1,14 @@
-﻿namespace Core.Interfaces.Models
+﻿using System;
+
+namespace Core.Interfaces.Models
 {
-    public interface IBaseEntity : ITrackEntity
+    public interface IBaseEntity //: ITrackEntity
     {
         string Id { get; set; }
         string GUId { get; set; }
+        DateTime CreatedDate { get; set; }
+        DateTime ModifiedDate { get; set; }
+        string CreatedBy { get; set; }
+        string ModifiedBy { get; set; }
     }
 }
